831835c44eeb54dbc22c3ddb2d455891315c2250
Riabtsev  Viktor <riabtse1@ug244.eecg>
2014-11-13 19:31:23 -0500

    Interface for symbol

--- lab3/symbol.h
+++ lab3/symbol.h
@@ -1,18 +1,29 @@
 #ifndef _SYMBOL_H
 #define _SYMBOL_H
 
-struct stack_elem;
+typedef struct _symbol_table_entry {
+	char *id;
+	int is_const;
+	int type_code;
+	int vec;
+	int is_init;
+}symbol_table_entry;
 
-void stack_push(node *);
+void stack_push();
 node * stack_pop();
 node * stack_top();
 int stack_is_empty();
 int stack_size();
 
-void symbol_add(char *id, int is_const ,int type_code, int vec, int initialised);
-int symbol_exists(char *id);
-int symbol_exists_in_this_scope(char *);
-int symbol_is_initialised(char *id);
+void scope_enter();
+void scope_exit();
+
+int symbol_exists_in_this_scope(char *id);
+void symbol_add(symbol_table_entry entry);
+
+symbol_table_entry* symbol_find(char *id);
+
+
 
 
 
