b54bcde36335b8c1aebeb3360dcb76332d8e5304
Riabtsev  Viktor <riabtse1@ug244.eecg>
2014-11-13 19:03:06 -0500

    moved type into overall struct2

--- lab3/symbol.h
+++ lab3/symbol.h
@@ -1,6 +1,19 @@
 #ifndef _SYMBOL_H
 #define _SYMBOL_H
 
+struct stack_elem;
+
+void stack_push(node *);
+node * stack_pop();
+node * stack_top();
+int stack_is_empty();
+int stack_size();
+
+void symbol_add(char *id, int is_const ,int type_code, int vec, int initialised);
+int symbol_exists(char *id);
+int symbol_exists_in_this_scope(char *);
+int symbol_is_initialised(char *id);
+
 
 
 #endif
