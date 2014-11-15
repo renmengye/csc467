f78f8b88602601994ab2e3aa2253264701056042
Mengye Ren <rmy@localhost.localdomain>
2014-11-14 22:52:48 -0500

    Finished symbol table.

--- lab3/symbol.c
+++ lab3/symbol.c
@@ -4,17 +4,78 @@
 
 #include "symbol.h"
 
-int scope_are_in_main(){
-	return 0;
+symbol_table_entry* stack = NULL;
+
+void scope_enter() {
+	symbol_table_entry *temp = malloc(sizeof(symbol_table_entry));
+	temp->next = stack;
+	temp->sib = NULL;
+	temp->id = NULL;
+	stack = temp;
 }
 
-int symbol_exists_in_this_scope(char *id){
-	return 0;
+void scope_exit() {
+	symbol_table_entry *cur = stack;
+	stack = stack->next;
+	while(cur) {
+		symbol_table_entry *next = cur->sib;
+		free(cur);
+		cur = next;
+	}
 }
-void symbol_add(symbol_table_entry entry){
 
+void symbol_cpy(symbol_table_entry *src, symbol_table_entry *dst) {
+	dst->id = src->id;
+	dst->is_const = src->is_const;
+	dst->type_code = src->type_code;
+	dst->vec = src->vec;
+	dst->is_init = src->is_init;
+}
+
+void symbol_add(symbol_table_entry entry) {
+	symbol_table_entry *temp = malloc(sizeof(symbol_table_entry));
+	if(!stack) {
+		stack = malloc(sizeof(symbol_table_entry));
+		stack->sib = NULL;
+		stack->next = NULL;
+		stack->id = NULL;
+	}
+	temp->next = stack->next;
+	temp->sib = stack;
+	symbol_cpy(&entry, temp);
+	stack = temp;
+}
+
+int scope_is_in_main(){
+	return stack ? stack->next == NULL : 0;
+}
+
+int symbol_exists_in_this_scope(char *id) {
+	symbol_table_entry *cur = stack;
+	while(cur) {
+		if (cur->id) {
+			if(strcmp(cur->id, id) == 0) {
+				return 1;
+			}
+		}
+		cur = cur->sib;
+	}
+	return 0;
 }
 
 symbol_table_entry* symbol_find(char *id){
+	symbol_table_entry *cur = stack;
+	while(cur) {
+		symbol_table_entry *cur2 = cur;
+		while(cur2) {
+			if (cur2->id) {
+				if(strcmp(cur2->id, id) == 0) {
+					return cur2;
+				}
+			}
+			cur2 = cur2->sib;
+		}
+		cur = cur->next;
+	}
 	return NULL;
 }
--- lab3/symbol.h
+++ lab3/symbol.h
@@ -1,15 +1,15 @@
 #ifndef _SYMBOL_H
 #define _SYMBOL_H
 
-
-
 typedef struct _symbol_table_entry {
 	char *id;
 	int is_const;
 	int type_code;
 	int vec;
 	int is_init; //VERY IMPORTANT, the implementation for this is not straight forward
-}symbol_table_entry;
+	struct _symbol_table_entry *next;
+	struct _symbol_table_entry *sib;
+} symbol_table_entry;
 
 //Depending on the size of the vector, say 4, the is_init can be in binary:
 // 0000 for fully unitialized, 1000, means the 4th, ie id[3] has been initialized
@@ -18,28 +18,12 @@ typedef struct _symbol_table_entry {
 //Doesnt change anything else, since you end up returning a pointer to data entry anyway,
 //so I can edit the contetns to my hearts content
 
-void stack_push();
-void * stack_pop();
-void * stack_top();
-int stack_is_empty();
-int stack_size();
-
 void scope_enter();
 void scope_exit();
-
-//MORE STUFF. I NEED ONE MORE FUNCTION
-//One type of predefined variables: result, can only be assigned in the main block and
-//note in any if or else block.
 int scope_is_in_main();
-
 int symbol_exists_in_this_scope(char *id);
 void symbol_add(symbol_table_entry entry);
-
 symbol_table_entry* symbol_find(char *id);
-
-
-
-
 
 #endif
 
